798862b7aa2cf977a080f1af77594825d9cacfd8
douglas oliveira <douglasgama0@gmail.com>
2022-01-10 13:02:44 -0300

    fix: ajustando header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import "./header.css";
-import { Link, Route } from "react-router-dom";
+import { Link, } from "react-router-dom";
+
+import logoSvg from "../elementsPage/icons/header/VectorlogoOriginal.svg";
+import searchIcon from "../elementsPage/icons/header/ic_search_black_24pxsearch.svg";
+import cartIcon from "../elementsPage/icons/header/ic_shopping_cart_black_24pxbag.svg";
 
 export default class Header extends Component {
   render() {
     return (
-      <Route>
+      <>
         <div className="header-page">
           <Link to="/home">
-            {/* <img src={ logoSvg } alt="logo-Original-io" /> */ }
+            { <img src={ logoSvg } alt="logo-Original-io" /> }
           </Link>
         </div>
         <div className="nav-links">
@@ -22,15 +26,15 @@ export default class Header extends Component {
             <Link to="/acessorios" className="link routers">ACESSÓRIOS</Link>
           </div>
           <div>
-            {/* <img src={ searchIcon } alt="icon-search" width="21px" /> */ }
+            { <img src={ searchIcon } alt="icon-search" width="21px" /> }
             <input className="search-input" type="text" placeholder="Busca" />
             <Link to="carrinho" className="link">
-              {/* <img src={ cartIcon } alt="icon-bag" width="21px" /> */ }
+              { <img src={ cartIcon } alt="icon-bag" width="21px" /> }
               0
             </Link>
           </div>
         </div>
-      </ Route>
+      </>
     );
   }
 }
